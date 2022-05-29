67b111c9835e1146204f64857eefb8e110af61ef
jojaehee <jaehee3679@daum.net>
2022-05-29 10:50:46 +0900

    fff

--- 7.built-in/1.wrapper.js
+++ 7.built-in/1.wrapper.js
@@ -1,6 +1,6 @@
 // 래퍼 객체 (Wrapper Object)
 // 원시값을 필요에 따라서 관련된 빌트인 객체로 변환한다.
-const number = 123; // number 원시 타입
+const number = 123; // number 원시 타입  //number는 객체가 아닌데? -> 객체에서 함수를 썼던 것처럼 사용됨
 // number 원시타입을 감싸고 있는 Number 객체로 감싸짐
 console.log(number.toString());
 console.log(number); // number 원시 타입

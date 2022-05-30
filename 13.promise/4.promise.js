38288d0f8740a33d82ff64b9edfc988386c5ee9f
jojaehee <jaehee3679@daum.net>
2022-05-30 13:29:25 +0900

    ddd

--- 13.promise/4.promise.js
+++ 13.promise/4.promise.js
@@ -9,5 +9,5 @@ function runInDelay(seconds) {
 
 runInDelay(2)
   .then(() => console.log('타이머 완료!'))
-  .catch(console.error)
+  .catch(console.error) //이거 원래 catch(error => console.errror(error)) 였는데 생략된 것임 인풋값이랑 들어가는 인자값이랑 다 똑같아서.
   .finally(() => console.log('끝났다!'));
--- 13.promise/7.async.js
+++ 13.promise/7.async.js
@@ -28,10 +28,16 @@ function fetchFruits(){
   return getBanana() //
   .then ((banana)=>
    getApple()
-          .then((apple)=>[banana,apple])
+          .then((apple)=>[banana,apple]) // 다 return한 promise가 다 resolve가 되면~! 
   );
 }
 
+
+//결국 이 함수는 promise를 return하게 된다는 것을 알 수 있음. 
+
+fetchFruits().then(result=>console.log(result));
+
+//비동기적인걸 동기적으로 보이게끔 해보자 -> 즉 then을 사용하지 않고도 절차적으로 보이게끔 할 수 있음. 
 
 //call back 계속 하면 여전히 지저분함 
 // 
@@ -41,6 +47,7 @@ function fetchFruits(){
 
 //promise니까 aswait를 꼭 할당해준다 
 // 바나나과 사과를 같이 가지고 오기
+//async 붙이면 이 코드는 비동기적인 함수야~ 이제 동기적으로 작성할 수 있게 됨 
 async function fetchFruits() { // 이 함수는 비동기 함수야! -> async 는 바나나와 
   const banana = await getBanana(); //바나나 가져올떄까지 기다려라! 
   const apple = await getApple(); // apple에는 promise가 들어있음. getapple의 promise가 끝날 떄까지 기다렸다가 값 자체를 apple 에 할당함 
